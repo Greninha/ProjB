2b33cc271c4a71b5b61819c600fccc67f91aa1c4
Greninha <60706959+Greninha@users.noreply.github.com>
2020-02-14 10:39:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <center><h1><strong><a href="index.php">Bora Nois </a></a></strong></h1></center>
-
+//Everson é gay
 <form name="formulario" id="formulario" method="POST" action="php.php">
 <label>Digite um Número: </label>
 <input name="n" id="n" type="text" place placeholder="Digite um número"><br/>
